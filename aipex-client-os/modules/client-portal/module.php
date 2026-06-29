b484914cad5a44ec73f169f51f40de79d6c87345
Aipex Solutions <info@theweb4less.co.uk>
2026-06-29 21:56:27 +0100

    Add client portal module placeholder

---
+++ aipex-client-os/modules/client-portal/module.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Client Portal module.
+ *
+ * MVP scope:
+ * - Client dashboard
+ * - Messages
+ * - Uploads
+ * - Tasks
+ * - Workflow progress
+ */
