e4c381b67f46355ea17ec3976dc795d4a8c920b2
Aipex Solutions <info@theweb4less.co.uk>
2026-06-29 21:56:43 +0100

    Add secure vault module placeholder

---
+++ aipex-client-os/modules/secure-vault/module.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Secure Vault module.
+ *
+ * MVP scope:
+ * - Encrypted client access details
+ * - Document references
+ * - Recovery notes
+ * - Audit trail
+ */
