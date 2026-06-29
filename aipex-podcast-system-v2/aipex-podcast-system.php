ae77fafe36e8206b26e188c3cda26d911d33d6ed
Aipex Solutions <info@theweb4less.co.uk>
2026-06-29 16:58:44 +0100

    Load context filtering fixes

--- aipex-podcast-system-v2/aipex-podcast-system.php
+++ aipex-podcast-system-v2/aipex-podcast-system.php
@@ -2,12 +2,12 @@
 /**
  * Plugin Name: Aipex Podcast System v2
  * Description: Modular podcast CMS with ACF fields, shortcodes, Elementor widgets, scanners and Dropbox importer.
- * Version: 2.2.5
+ * Version: 2.2.6
  * Author: Aipex
  */
 if (!defined('ABSPATH')) exit;
 
-define('AIPEX_PODCAST_VERSION','2.2.5');
+define('AIPEX_PODCAST_VERSION','2.2.6');
 define('AIPEX_PODCAST_FILE',__FILE__);
 define('AIPEX_PODCAST_DIR',plugin_dir_path(__FILE__));
 define('AIPEX_PODCAST_URL',plugin_dir_url(__FILE__));
@@ -21,6 +21,7 @@ require_once AIPEX_PODCAST_DIR.'includes/class-admin.php';
 require_once AIPEX_PODCAST_DIR.'includes/class-dropbox.php';
 require_once AIPEX_PODCAST_DIR.'includes/class-elementor.php';
 require_once AIPEX_PODCAST_DIR.'includes/class-migration.php';
+require_once AIPEX_PODCAST_DIR.'includes/class-context-fixes.php';
 
 register_activation_hook(__FILE__, function(){
     Aipex_Podcast_Post_Types::register();
@@ -28,3 +29,4 @@ register_activation_hook(__FILE__, function(){
 });
 register_deactivation_hook(__FILE__, function(){ flush_rewrite_rules(); });
 add_action('plugins_loaded', ['Aipex_Podcast_Core','init']);
+add_action('plugins_loaded', ['Aipex_Podcast_Context_Fixes','init'], 20);
