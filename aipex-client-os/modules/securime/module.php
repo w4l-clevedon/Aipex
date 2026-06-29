7c552a0ffd9355529f402e07da72939f052f8bb4
Aipex Solutions <info@theweb4less.co.uk>
2026-06-29 21:56:50 +0100

    Add Securime module placeholder

---
+++ aipex-client-os/modules/securime/module.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Securime module.
+ *
+ * Builds on Secure Vault.
+ * MVP scope:
+ * - Check-in tracking
+ * - Inactivity alerts
+ * - Escalation contacts
+ * - Emergency release workflow
+ * - Full audit trail
+ */
