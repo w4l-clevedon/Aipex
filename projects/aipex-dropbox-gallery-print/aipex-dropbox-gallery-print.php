50cf1afd29caabb6ae68989adac0cba0efe11fa2
Aipex Solutions <info@theweb4less.co.uk>
2026-07-12 21:33:25 +0100

    Scaffold Dropbox Gallery & Print plugin

---
+++ projects/aipex-dropbox-gallery-print/aipex-dropbox-gallery-print.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: Aipex Dropbox Gallery & Print
+ * Description: Dropbox-synchronised Elementor galleries with WooCommerce print personalisation.
+ * Version: 0.1.0
+ * Author: Aipex
+ * Text Domain: aipex-dropbox-gallery-print
+ * Requires at least: 6.5
+ * Requires PHP: 8.1
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+define( 'AIPEX_DGP_VERSION', '0.1.0' );
+define( 'AIPEX_DGP_FILE', __FILE__ );
+define( 'AIPEX_DGP_PATH', plugin_dir_path( __FILE__ ) );
+define( 'AIPEX_DGP_URL', plugin_dir_url( __FILE__ ) );
+
+require_once AIPEX_DGP_PATH . 'includes/class-plugin.php';
+
+register_activation_hook( __FILE__, array( 'Aipex_DGP\\Plugin', 'activate' ) );
+
+add_action(
+    'plugins_loaded',
+    static function (): void {
+        Aipex_DGP\Plugin::instance()->boot();
+    }
+);
